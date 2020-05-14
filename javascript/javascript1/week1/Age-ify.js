38240a4715d7656aba7830917766984d53f6858e
Elia-Git <holeselv17@gmail.com>
2020-05-14 14:42:33 +0200

    dog age estimator deleted

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,4 +1,6 @@
-const yearOfBirth = 1987;
-let yearFuture = 2078;
-let age = (yearFuture - yearOfBirth);
-console.log(age);
+// Age calculator code
+
+const yearOfBirth = 1987; // use const as birth year is fixed
+let yearFuture = 2078; // use let as it can be assiged to different values
+const age = (yearFuture - yearOfBirth);
+console.log('You will be ' + age + ' years old in ' + yearFuture);
--- javascript/javascript1/week1/Goodboy-Oldboy.js
+++
@@ -1,4 +0,0 @@
-const dogYearOfBirth = 2020;
-let dogYearFuture = 2055;
-
-let shouldShowResultInDogYears;
