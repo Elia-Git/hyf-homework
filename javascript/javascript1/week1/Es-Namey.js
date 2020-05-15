f25e257e8c23a5208b1cda2aa73cc4d92ef75acf
Elia-Git <holeselv17@gmail.com>
2020-05-15 12:08:12 +0200

    Es-Namey js file removed

--- javascript/javascript1/week1/Es-Namey.js
+++
@@ -1,8 +0,0 @@
-const firstWords = [OlaCabs, AddressHealth, Zomato, Paytm, FreshToHome];
-const secondWords = [FreshMenu, Flyrobe, Myra, Shuttl, Dunzo];
-
-let startupName = [firstWords[1], conct(secondWords[0])];
-
-const randomNumber = Math.floor(Math.random() * 10) + 0;
-
-console.log(startupName[0]);
