1cfbfa80638be69a44ca44d3ab7ad20635d2ae3d
Elia-Git <holeselv17@gmail.com>
2020-05-15 09:52:48 +0200

    Goodboy-Oldboy js file finally modified

--- javascript/javascript1/week1/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -2,12 +2,17 @@ const dogYearOfBirth = 2020; // value do not change
 let dogYearFuture = 2055; // Value can change with the same variable name
 let dogYear = dogYearFuture - dogYearOfBirth;
 
-var shouldShowResultInDogYears = 'true';
+var shouldShowResultInDogYears = "true";
 
 //To test if the else condition works
-//var shouldShowResultInDogYears = 'false';
+//var shouldShowResultInDogYears = "false";
 
-if (shouldShowResultInDogYears == 'true') {
-    console.log('Your dog will be ' + dogYear + ' dog years old in ' + dogYearFuture);
-
-} else { console.log('Your dog will be ' + dogYear * 7 + ' human years old in ' + dogYearFuture); }
+if (shouldShowResultInDogYears == "true") {
+    console.log(
+        "Your dog will be " + dogYear + " dog years old in " + dogYearFuture
+    );
+} else {
+    console.log(
+        "Your dog will be " + dogYear * 7 + " human years old in " + dogYearFuture
+    );
+}
