e9a13ca2da4d645c06258361f30f00853016863f
Elia-Git <holeselv17@gmail.com>
2020-05-16 21:50:44 +0200

    My age calcultor Age-ify js file modified

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,6 +1,6 @@
 // Age calculator code
 
 const yearOfBirth = 1987; // use const as birth year is fixed
-let yearFuture = 2045; // use let as it can be assiged to different values
+const yearFuture = 2045; // use const as it will not be re-assign to a new value
 const age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);
