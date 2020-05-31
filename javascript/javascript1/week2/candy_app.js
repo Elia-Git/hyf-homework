6a4f94d9c74269c74be3a4a578e03dc2b7ac5ec3
Elia-Git <holeselv17@gmail.com>
2020-05-31 23:23:04 +0200

    Feedback implemented to candy_app

--- javascript/javascript1/week2/candy_app.js
+++ javascript/javascript1/week2/candy_app.js
@@ -13,14 +13,15 @@ function addCandy(candyType, weight) {
     } else if (candyType === "toffee") {
         boughtCandyPrices.push(1.1 * weight);
     } else {
-        let a = "Candy category not found";
-        console.log(a);
+        console.log(" \n-------------"); // to display the results seperately
+        console.log("Candy category not found");
     }
 }
 addCandy("sweet", 7);
 addCandy("chocolate", 10);
 addCandy("chewing-gum", 8);
 addCandy("toffee", 6);
+console.log(" \n-------------");
 console.log(boughtCandyPrices);
 
 // If I buy more candies?
@@ -32,13 +33,15 @@ function canBuyMoreCandy() {
     let x = 0;
     while (x < boughtCandyPrices.length) {
         amountSpent += boughtCandyPrices[x];
-        ++x;
-        console.log("Shoping done" + amountSpent);
-        return amountSpent < amountTospend;
+        x++;
+        console.log("Shoping done " + amountSpent);
     }
-    if (canBuyMoreCandy()) {
-        console.log("You can buy more, so go ahead!");
-    } else {
-        console.log("Enough candy for you");
-    }
+    return amountSpent < amountTospend;
+}
+if (canBuyMoreCandy()) {
+    console.log(" \n-------------");
+    console.log("You can buy more, so go ahead!");
+} else {
+    console.log(" \n-------------");
+    console.log("Enough candy for you");
 }
