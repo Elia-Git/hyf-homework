9a0c4016cc57a4eddf143d7b8aaeb2a810af09f2
Elia-Git <holeselv17@gmail.com>
2020-06-01 02:23:04 +0200

    Feedback implemented on class_creator JS file

--- javascript/javascript1/week2/class_creator.js
+++ javascript/javascript1/week2/class_creator.js
@@ -1,39 +1,37 @@
-// copy your homework solution here to make the webpage work!
 const class07Students = ["Bob", "Jordi", "Klaus", "Jonas", "Mike", "Erik"];
-let check;
 let number;
+let x;
 
 function addStudentToClass(studentName) {
     // You write code here
-    const queen = "queen";
-    // for loop
-    for (let x = 0; x < class07Students.length; ++x) {
-        number = number + class07Students[x];
+    for (let x = 0; x < class07Students.length; x++) {
+        number += class07Students[x];
     }
-    if (class07Students.length < 6) {
-        check = class07Students.every(function(v) {
-            return v != studentName;
-        });
-        if (check == true) {
-            class07Students.push(studentName);
-        } else if (studentName == queen);
-        class07Students.push(queen);
+    if (class07Students.includes(studentName)) {
+        console.log("\n if else conditions output()\n---------");
+        console.log("This student name " + studentName + " is already registered in the class");
+    } else if (studentName === "") {
+        console.log("You can not add an empty string to the class");
+    } else if (studentName === "queen") {
+        class07Students.push(studentName);
+    } else if (class07Students.length = 6) {
+        console.log("This Class is fully booked and only the Queen is has a place");
+    } else {
+        return class07Students.length;
     }
 }
 
 function getNumberOfStudents() {
-    // You write code here
-
-    for (let x = 0; x < 6; x++) {
-        class07Students.length;
-        if (class07Students.length === 6) {
-            return "This Class is fully booked and only the Queen is has a place";
-        } else if (check !== true) {
-            return "This student name is already registered in the class";
-        }
-    }
+    // You code here
     return class07Students.length;
 }
+// It works for all this conditions bellow
+addStudentToClass("Erik"); // if we can add twice
+addStudentToClass(""); // if it can add an empty string
+addStudentToClass("Tom"); // if we can add more than 6
+addStudentToClass("queen"); // what if we add the queen any time*/
 
+
+console.log("\n function output()\n---------");
 console.log("List of students in the class:", class07Students);
 console.log("Maximum number of students is:", class07Students.length);
