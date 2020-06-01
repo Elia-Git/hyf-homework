ca34950c766cc9e628ccbad051ab301eeb77ab4e
Elia-Git <holeselv17@gmail.com>
2020-06-01 03:48:49 +0200

    Feedback implemnted to Item removal file

--- javascript/javascript1/week3/item-removal.js
+++ javascript/javascript1/week3/item-removal.js
@@ -11,15 +11,12 @@ const names = [
 const nameToRemove = "Ahmad";
 
 // Write some code here
-names.splice(1, 1); // Splice is special thing allows us add and remove an element;
-
-// Options but not recomended!
-// delete names[1]; This will return "empty" with 5 characters-I dont like it
-// ['Peter', <empty>,'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
-
-// names[1]=null will return null with 4 characters
-// ['Peter', null,'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
-
+for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    if (name === nameToRemove) {
+        names.splice(i, 1);
+    }
+}
 // Code done
 
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
