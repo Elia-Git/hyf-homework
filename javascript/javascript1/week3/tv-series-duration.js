280b6ee6cf0745b2ec434cfa9291abb4f67c20b3
Elia-Git <holeselv17@gmail.com>
2020-06-01 03:36:39 +0200

    Feedback Implemeted to Tv Series File

--- javascript/javascript1/week3/tv-series-duration.js
+++ javascript/javascript1/week3/tv-series-duration.js
@@ -26,29 +26,19 @@ const seriesDurations = [
     }
 ]
 
-function logOutSeriesText(info) {
-    // variables declaration
-    let daysTotal;
-    let hoursTotal;
-    let minutesTotal;
-    let seriesInTotal;
-    let total = 0;
 
-    for (let i = 0; i < info.length; ++i) {
-        //Days to year in %age in 80 years
-        daysTotal = (info[i].days / (365.6 * 80)) * 100;
-        //Hours to year in %age in 80 years
-        hoursTotal = (info[i].hours / (365.6 * 80 * 24)) * 100;
-        //Minutes to year in %  age in 80 years
-        minutesTotal = (info[i].minutes / (365.6 * 80 * 24 * 60)) * 100;
-        // Total % of age for a single series in 80 years
-        seriesInTotal = daysTotal + hoursTotal + minutesTotal;
-        console.log(info[i].title + ' took me ' + seriesInTotal.toFixed(2) + '% of my life');
-        // Iterating total % of age for all series in 80 years
-        total += seriesInTotal;
+function logOutSeriesText(seriesDurations) {
+    const lifespan = 80 * 365 * 24 * 60; // minutes
+    let total = 0;
+    for (let i = 0; i < seriesDurations.length; i++) {
+        const seriesDuration = seriesDurations[i].days * 24 * 60 + seriesDurations[i].hours * 60;
+        const percent = (seriesDuration / lifespan) * 100;
+        total += percent;
+        console.log(`${seriesDurations[i].title} took ${percent.toFixed(3)} % of my life`);
     }
-    console.log(
-        'In total that is ' + total.toFixed(3) + " of my life took only by those 4 tv-series ");
+    console.log(`In total that is ${total.toFixed(3)} % of my life`);
 }
+
+
 // Logs out the text by calling the function
 logOutSeriesText(seriesDurations);
