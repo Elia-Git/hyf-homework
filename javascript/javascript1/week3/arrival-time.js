4386372b19a78aa1cf46f935d1390ac2f857cfc4
Elia-Git <holeselv17@gmail.com>
2020-06-01 03:43:33 +0200

    Feedback Implemented to Arrival Time file

--- javascript/javascript1/week3/arrival-time.js
+++ javascript/javascript1/week3/arrival-time.js
@@ -9,11 +9,8 @@ function findTime(information) {
 
 const travelTime = findTime(travelInformation);
 let hours = Math.floor(travelTime);
-let minutes = Math.ceil((travelTime % Math.floor(travelTime)) * 100);
-if (minutes >= 60) {
-    hours += Math.floor(minutes / 60);
-    minutes -= 60;
-}
+let minutes = Math.ceil((travelTime % Math.floor(travelTime)) * 60);
+
 console.log(
     `The time needed to arrive our destination is ${hours} hours and ${minutes} minutes`
 ); // Gives out 7 hours and 17 minutes
