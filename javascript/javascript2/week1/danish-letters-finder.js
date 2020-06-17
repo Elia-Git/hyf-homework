900d96a6ee4d81e1ac33530cbf6cd4f5b32a07e0
Elia-Git <holeselv17@gmail.com>
2020-06-17 11:20:00 +0200

    Danish letter finder Feedback implemeted

--- javascript/javascript2/week1/danish-letters-finder.js
+++ javascript/javascript2/week1/danish-letters-finder.js
@@ -1,17 +1,17 @@
 function findDanishLetters(str) {
-    let countA = 0;
-    let countB = 0;
-    let countC = 0;
+    let countÅ = 0; // counts letters å
+    let countØ = 0; // counts letters ø
+    let countÆ = 0; // counts letters æ
     let total = 0;
     for (let i = 0; i < str.length; i++) {
         if (str[i] == "å" || str[i] == "Å") {
-            countA += 1;
+            countÅ += 1;
             total += 1;
         } else if (str[i] == "ø" || str[i] == "Ø") {
-            countB += 1;
+            countØ += 1;
             total += 1;
         } else if (str[i] == "æ" || str[i] == "Æ") {
-            countC += 1;
+            countÆ += 1;
             total += 1;
         }
     }
@@ -24,8 +24,12 @@ function findDanishLetters(str) {
             str.toUpperCase().indexOf("Æ") === -1)
     )
         return "Sorry, No danish letters found";
-    else return { total: total, å: countA, ø: countB, æ: countC };
+    else {
+        return { total: total, å: countÅ, ø: countØ, æ: countÆ };
+    }
 }
+
 // works for both lower and upper case
-console.log(findDanishLetters("Jeg har en blå bil Åå Øø ")); //{ total: 5, 'å': 3, 'ø': 2, 'æ': 0 }
+
+console.log(findDanishLetters("Jeg har en blå Åå Øø ")); //{ total: 5, 'å': 3, 'ø': 2, 'æ': 0 }
 console.log(findDanishLetters("Blå grød med røde bær Æ æ")); //{ total: 6, 'å': 1, 'ø': 2, 'æ': 3 }
