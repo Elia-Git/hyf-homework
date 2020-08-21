4c2090440bff0ce513f8bf0f2a517a48c0f77128
Elia-Git <holeselv17@gmail.com>
2020-08-22 00:49:53 +0200

    Movies Feedback Implemenyted

--- javascript/javascript3/week2/Movies/movies_array.js
+++ javascript/javascript3/week2/Movies/movies_array.js
@@ -1,39 +1,19 @@
 //1. Create an array of bad movies
-const apiUrl = `https://tinyurl.com/y9zck5xx`;
-// Bad movies rated for <=4
-async function badMoviesArray() {
-    //a.fetching the movies data
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((movies) => {
-            //b.filtering bad movies accrotding to ratings
-            const badMovies = movies
-                .filter((movie) => movie.rating <= 4)
-                .map((movie) => movie);
-            console.log(badMovies);
-        })
-        .catch((error) => {
-            //b.Throw an error if above condition failed
-            console.log(error);
-        });
-}
-badMoviesArray();
 
-//2. Create an array of bad movies since year 2000
+const moviesPromise = fetch(`https://tinyurl.com/y9zck5xx`);
 
-const badMoviesSince2000 = () => {
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((movies) => {
-            //a.Check both if movies are bad & are produced since 2000
-            const badMoviesAfter2000 = movies
-                .filter((movie) => movie.rating <= 4 && movie.year > 2000)
-                .map((movie) => movie);
-            console.log(badMoviesAfter2000);
-        })
-        .catch((error) => {
-            //b.Throw an error if above condition failed
-            console.error(error);
-        });
-};
-badMoviesSince2000();
+moviesPromise
+    .then((response) => response.json())
+    .then((movies) => {
+        //1. Create an array of bad movies
+        const badMovies = movies.filter((movie) => movie.rating <= 4);
+        console.log(badMovies);
+        //2. Create an array of bad movies since year 2000
+        const badMoviesSince2000 = movies.filter(
+            (movie) => movie.rating <= 4 && movie.year > 2000
+        );
+        console.log(badMoviesSince2000);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
