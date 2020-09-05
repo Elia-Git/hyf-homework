817bbf917cb46cb1d287060081a2db173a306a51
Elia-Git <61273043+Elia-Git@users.noreply.github.com>
2020-09-05 03:46:30 +0200

    wrong file from old deleted branch

--- nodejs/week1/homework/src/backend/routes/reservations.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const reservations = require("../data/reservations.json");
-
-router.get("/", async(request, response) => {
-    response.json(reservations);
-});
-module.exports = router;
