958d67902ebf9b9d8ef28aa8221635f82bbf55be
Elia-Git <holeselv17@gmail.com>
2020-09-28 10:23:31 +0200

    nodejs/week1 merged

--- nodejs/week1/homework/src/backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const port = 3000;
-const hostname = "localhost";
-
-app.listen(port, () => {
-    console.log(
-        `Hello Elias: -Here is the homework server runing!! http://${hostname}:${port}/`
-    );
-});
